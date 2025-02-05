2cb4d98d734e19a67332b3e8eabcb12d954db289
Dmitry Nikolaev <D.Nikolaev94@mail.ru>
2025-02-05 23:07:13 +0500

    исправил проблемы с фильтрацией

--- backend/foodgram_backend/api/filters.py
+++ backend/foodgram_backend/api/filters.py
@@ -17,10 +17,10 @@ class RecipeFilter(django_filters.FilterSet):
     tags = django_filters.AllValuesMultipleFilter(
         field_name='tags__slug'
     )
-    is_favorited = django_filters.BooleanFilter(
+    is_favorited = django_filters.NumberFilter(
         method='get_is_favorited'
     )
-    is_in_shopping_cart = django_filters.BooleanFilter(
+    is_in_shopping_cart = django_filters.NumberFilter(
         method='get_is_in_shopping_cart'
     )
     author = django_filters.AllValuesMultipleFilter(
@@ -32,11 +32,11 @@ class RecipeFilter(django_filters.FilterSet):
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
-        if value:
+        if self.request.user.is_authenticated and value:
             return queryset.filter(favorite__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
+        if self.request.user.is_authenticated and value:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
