a12bc2dffa476672e1496edfbf27141b9f54878f
Dmitry Nikolaev <D.Nikolaev94@mail.ru>
2025-07-04 20:43:35 +0500

    Поменял структуру api

--- backend/api/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-from foodgram_backend import constants
-
-
-class CustomPagination(PageNumberPagination):
-    page_size_query_param = 'limit'
-    page_size = constants.MAX_PAGE_SIZE
